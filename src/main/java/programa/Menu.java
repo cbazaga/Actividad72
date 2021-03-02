251a519bac3a1f22fb06810826d2f2bf1afceb04
ciclost <ciclost@pc22>
2021-03-02 18:51:17 +0100

    Empezando A73

---
+++ src/main/java/programa/Menu.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programa;
+
+/**
+ *
+ * @author ciclost
+ */
+public class Menu {
+    
+    
+    public static void menu(){
+        int numero=-1;
+        System.out.println("                         Menú    ");
+        System.out.println("------------------------------------------------------");
+        System.out.println("1.Alta y recepción de vehículos:");
+        System.out.println("2.Reclamar vehículo para entrar en box:");
+        System.out.println("3.Mover todos los vehículos de fase dentro de un box:");
+        System.out.println("4.Información del estado de un box concreto:");
+        System.out.println("5.Información general de todos los boxes:");
+        System.out.println("6.Salir:");
+    }
+}
